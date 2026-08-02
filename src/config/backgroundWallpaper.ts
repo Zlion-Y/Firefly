74bb5c5c48a7d4bc307cf63933fc8f2a8b17fb77
ynbl07 <115333692+ynbl07@users.noreply.github.com>
2026-08-02 15:53:54 +0800

    更新 backgroundWallpaper.ts

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -104,7 +104,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			transparentMode: "semi",
 			// 毛玻璃模糊度，0 即关闭导航栏的毛玻璃
 			// 注意：导航栏子菜单与浮动面板始终保留毛玻璃，模糊度跟随此项但有最小值
-			blur: 2,
+			blur: 7,
 		},
 		// 水波纹动画效果配置，开启会影响页面性能，请根据自己的喜好开启
 		waves: {
