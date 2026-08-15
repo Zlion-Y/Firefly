fa0b46751ac2e20c3715e482f17909ad7b9f82d5
ynbl07 <2841825025@qq.com>
2026-08-15 14:54:26 +0800

    增加天气组件

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -69,6 +69,16 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			showOnPostPage: false,
 		},
 		{
+			// 组件类型：天气组件
+			type: "weather",
+			// 是否启用该组件
+			enable: true,
+			// 组件位置
+			position: "sticky",
+			// 是否在文章详情页显示
+			showOnPostPage: true,
+		},
+		{
 			// 组件类型：分类组件
 			type: "categories",
 			// 是否启用该组件
@@ -339,6 +349,14 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 					unknownBuildPlatform: "Unknown CI",
 				},
 			},
+		},
+		{
+			// 组件类型：天气组件
+			type: "weather",
+			// 是否启用该组件
+			enable: true,
+			// 是否在文章详情页显示
+			showOnPostPage: true,
 		},
 	],
 };
