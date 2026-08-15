bb8cac431414e7251f46c3fe1170b4533a1343b4
ynbl07 <2841825025@qq.com>
2026-08-15 15:01:02 +0800

    更新 sidebarConfig.ts

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -76,7 +76,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件位置
 			position: "sticky",
 			// 是否在文章详情页显示
-			showOnPostPage: true,
+			showOnPostPage: false,
 		},
 		{
 			// 组件类型：分类组件
@@ -112,7 +112,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：站点统计组件
 			type: "stats",
 			// 是否启用该组件
-			enable: true,
+			enable: false,
 			// 组件位置
 			position: "sticky",
 			// 是否在文章详情页显示
