1d52c48cde1b74082823d835c221470b0deac1b4
ynbl07 <2841825025@qq.com>
2026-08-17 18:33:56 +0800

    更新 musicConfig.ts

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -9,7 +9,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	showInSidebar: true,
 
 	// 使用方式："meting" 使用 Meting API，"local" 使用本地音乐列表
-	mode: "meting",
+	mode: "local",
 
 	// 默认音量 (0-1)
 	volume: 1,
@@ -18,7 +18,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	playMode: "random",
 
 	// 是否显启用歌词
-	showLyrics: false,
+	showLyrics: true,
 
 	// Meting API 配置
 	meting: {
