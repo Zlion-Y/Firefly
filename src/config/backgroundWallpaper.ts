61962a500be8d43db08ec7bb34f7ac15e66a3677
Zlion-Y <2841825025@qq.com>
2026-09-10 14:29:08 +0800

    更新 backgroundWallpaper.ts

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -44,7 +44,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		//	"assets/images/DesktopWallpaper/jianlai.jpg",
 		//	"assets/images/DesktopWallpaper/mountain.jpg",
 			"https://uapis.cn/api/get-image-bing-daily?format=image&resolution=1920&mkt=zh-CN",
-		,
+		
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: 
@@ -54,7 +54,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		//	"assets/images/MobileWallpaper/ship.png",
 		//	"assets/images/MobileWallpaper/tree2m.avif",
 			"https://uapis.cn/api/get-image-bing-daily?format=image&resolution=1920&mkt=zh-CN",
-		,
+		
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组，参考上面壁纸配置）
 		// 支持远程视频URL，本地视频请放在 public/assets/videos/ 目录下
