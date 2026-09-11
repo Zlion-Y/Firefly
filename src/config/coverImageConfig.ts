edd3bdbcfcd3548a1937d8c0862853e5c66f4dc7
Zlion-Y <2841825025@qq.com>
2026-09-11 13:21:41 +0800

    更新 coverImageConfig.ts

--- src/config/coverImageConfig.ts
+++ src/config/coverImageConfig.ts
@@ -19,13 +19,13 @@ import type { CoverImageConfig } from "../types/coverImageConfig";
  */
 export const coverImageConfig: CoverImageConfig = {
 	// 是否在文章详情页显示封面图
-	enableInPost: true,
+	enableInPost: false,
 
 	// 是否使用标题和元数据叠加在封面上的布局
-	enableInPostOverlay: true,
+	enableInPostOverlay: false,
 
 	// 是否显示转圈圈加载动画，会替代掉LQIP
-	showLoading: false,
+	showLoading: true,
 
 	randomCoverImage: {
 		// 随机封面图功能开关
