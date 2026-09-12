51ffffa73c3dbdf8964ce9bfcdbc5bb9bcdf1b99
Zlion-Y <2841825025@qq.com>
2026-09-12 22:41:34 +0800

    更新 backgroundWallpaper.ts

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -154,9 +154,9 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		gradient: {
 			enable: {
 				// 桌面端是否启用渐变过渡
-				desktop: false,
+				desktop: true,
 				// 移动端是否启用渐变过渡
-				mobile: false,
+				mobile: true,
 			},
 			// 渐变高度
 			height: "10%",
